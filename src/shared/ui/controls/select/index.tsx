ae6ab8854cf31c5edd19e4776a9a610868adae05
deljuse <musindelus@gmail.com>
2026-04-22 20:38:46 +0300

    add tests

--- src/shared/ui/controls/select/index.tsx
+++ src/shared/ui/controls/select/index.tsx
@@ -1,16 +1,22 @@
-import type { HTMLAttributes, ReactElement } from 'react';
-import { twMerge } from 'tailwind-merge';
+import type { HTMLProps, ReactElement } from "react";
+import { twMerge } from "tailwind-merge";
 
-function Select({ className, ...rest }: HTMLAttributes<HTMLInputElement>): ReactElement {
+function Select({
+  className,
+  children,
+  ...rest
+}: HTMLProps<HTMLSelectElement>): ReactElement {
   return (
-    <input
+    <select
       data-testid="form-field"
       className={twMerge(
-        'bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body',
-        className
+        "bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs appearance-none",
+        className,
       )}
       {...rest}
-    />
+    >
+      {children}
+    </select>
   );
 }
 
